58b7ff2fcfea66149d50bfc0eeafc4f0c93c7845
NickMostovoi <nick211996@gmail.com>
2021-11-11 08:34:09 +0200

    functions_part_1: done

--- js/functions_part_1.js
+++ js/functions_part_1.js
@@ -4,8 +4,6 @@
 // Не использовать встроенные функции/методы
 
 
-
-
 // Создать функцию isUndefined которая принимает в качестве единственного входящего параметра значение любого типа данных
 // и возвращает true если значение равно undefined иначе возвращает false.
 // Пример работы:
@@ -18,8 +16,12 @@
 // isUndefined(5);
 // => false
 
-
-
+var isUndefined = function (object) {
+    if (undefined === object) {
+        return true;
+    }
+    return false;
+};
 
 // Создать функцию isNull которая принимает в качестве единственного входящего параметра значение любого типа данных
 // и возвращает true если значение равно null иначе возвращает false.
@@ -31,8 +33,12 @@
 // isNull(5);
 // => false
 
-
-
+var isNull = function (data) {
+    if (null === data) {
+        return true;
+    }
+    return false;
+};
 
 // Создать функцию isBoolean которая принимает в качестве единственного входящего параметра значение любого типа данных
 // и возвращает true если принимаемое значение является значением логического типа данных. В противном случае вернет false.
@@ -46,8 +52,12 @@
 // isBoolean(true);
 // => true
 
-
-
+var isBoolean = function (value) {
+    if (typeof value === "boolean") {
+        return 'true';
+    }
+    return 'false';
+}
 
 // Создать функцию size которая принимает в качестве единственного входящего параметра массив и возвращает количество элементов в массиве.
 // Пример работы:
@@ -56,8 +66,9 @@
 // size([7, 2, 3, 5, 1]);
 // => 5
 
-
-
+var size = function (list) {
+    return list.length;
+};
 
 // Создать функцию first которая принимает в качестве единственного входящего параметра массив произвольных значений и возвращает первое из них.
 // Пример работы:
@@ -66,8 +77,9 @@
 // first([9, 0, 4, 7, 2]);
 // => 9
 
-
-
+var first = function (list) {
+    return list[0];
+};
 
 // Создать функцию last которая принимает в качестве единственного входящего параметра массив произвольных значений и возвращает последнее из них.
 // Пример работы:
@@ -76,8 +88,9 @@
 // last([8, 2, 1, 7, 3]);
 // => 3
 
-
-
+var last = function (list) {
+    return list[list.length - 1];
+};
 
 // Создать функцию isEven которая возвращает true если число четное или false в противном случае.
 // Пример работы:
@@ -88,8 +101,12 @@
 // isEven(7);
 // => false
 
-
-
+function isEven(value) {
+    if (value % 2 === 0) {
+        return true;
+    }
+    return false;
+};
 
 // Создать функцию indexOf которая вернёт позицию, на которой находится элемент value в массиве array, или -1, если данный элемент не был найден.
 // Пример работы:
@@ -100,8 +117,16 @@
 // indexOf([7, 2, 3], 5);
 // => -1
 
-
-
+var indexOf = function (array, value) {
+    var index = 0;
+    while (index < array.length) {
+        if (array[index] === value) {
+            return index;
+        }
+        index++;
+    }
+    return -1;
+};
 
 // Создать функцию lastIndexOf которая паринимает два параметра (массив, значение) и ищет значение в массиве и возвращет его индекс, но делает это не с начала массива, а с его конца.
 // Т.е. возвращает позицию последнего вхождения значения в массиве значений. Иначе возвращает -1.
@@ -111,64 +136,78 @@
 // lastIndexOf([1, 2, 3, 1, 2, 3], 3);
 // => 5
 
-
-
+var lastIndexOf = function (array, value) {
+    return array.lastIndexOf(value);
+}
 
 // Создать функцию push которая принимает массив и произвольное значение и возвращает копию массива + произвольное значение (которое находится в конце массива)
 // Пример работы:
 // push([1, 2, 3, 4], 5);
 // => [1, 2, 3, 4, 5]
 
-
-
+var push = function (array, value) {
+    array[array.length] = value;
+    return array;
+}
 
 // Создать функцию unshift которая принимает массив и произвольное значение и возвращает копию массива + произвольное значение (которое находится в начале массива)
 // Пример работы:
 // unshift([1, 2, 3, 4], 5);
 // => [5, 1, 2, 3, 4]
 
-
-
+var unshift = function (array, value) {
+    array.unshift(value);
+    return array;
+}
 
 // Создать функцию pop которая принимает массив и возвращает копию массива без последнего значения.
 // Пример работы:
 // pop([1, 2, 3, 4]);
 // => [1, 2, 3]
 
-
-
+var pop = function (array) {
+    array.pop();
+    return array;
+}
 
 // Создать функцию shift которая принимает массив и возвращает копию массива без первого значения.
 // Пример работы:
 // shift([1, 2, 3, 4]);
 // => [2, 3, 4]
 
-
-
+var shift = function (array) {
+    array.shift();
+    return array;
+};
 
 // Создать функцию getPositiveNumbers которая принимает массив чисел и возвращает массив положительных чисел найденных в исходном массиве.
 // Пример работы:
 // getPositiveNumbers([10, -5, 100, -2, 1000]);
 // => [10, 100, 1000]
 
-
-
+var getPositiveNumbers = function (array) {
+    return array.filter(function (value) {
+        return value > 0;
+    });
+};
 
 // Создать функцию reverse которая принимает массив и возвращает копию входящего массива с элементами в обратном порядке.
 // Пример работы:
 // reverse([1, 'lol', 5, {}, []]);
 // => [[], {}, 5, "lol", 1]
 
-
-
+var reverse = function (array) {
+    return array.reverse();
+}
 
 // Создать функцию compact которая принимает в качестве единственного входящего параметра массив произвольных значений и возвращает копию массива без undefined значений.
 // Пример работы:
 // compact([10, 1, 4, 2, undefined, 3, null]);
 // => [10, 1, 4, 2, 3, null]
 
-
-
+var compact = function (array) {
+    return array.filter(Boolean);
+};
 
 // Создать функцию contains которая принимает два входящих параметра (массив значений простых типов данных и значение простого типа данных).
 // Функция вернет true если в массиве содержится определенное значение. Иначе функция вернет false.
@@ -176,39 +215,52 @@
 // contains([1, 2, 3], 3);
 // => true
 
-
-
+var contains = function (list, value) {
+    var index = 0;
+    while (index < list.length) {
+        if (list[index] === value) {
+            return true;
+        }
+        index++;
+    }
+    return false;
+};
 
 // Создать функцию without которая возвращает копию массива, в которой удалены все значения второго аргумента указанного при вызове функции.
 // Пример работы:
 // without([3, 6, 7, 'rere'], 6);
 // => [3, 7, 'rere']
 
-
-
+var without = function (array, value) {
+    var number = array.indexOf(value);
+    if (number >= 0) {
+        array.splice(number, 1);
+    }
+    return array;
+}
 
 // Создать функцию concat которая принимает два массива и возвращает новый массив состоящий из значений первого и второго.
 // Пример работы:
 // concat(['a', 'b', 'c'], ['d', 'e', 'f']);
 // => [ "a", "b", "c", "d", "e", "f" ]
 
-
-
+var concat = function (arr1, arr2) {
+    return arr1.concat(arr2);
+}
 
 // Создать функцию slice которая принимает 3 параметра. Массив и два числа (begin и end). Возвращает копию части исходного массива. Начиная с индекса begin и заканчивая индексом end включительно (или концом массива если параметр end отстутствует).
 // Пример работы:
 // slice(['ant', 'bison', 'camel', 'duck', 'elephant'], 2, 3);
 // => ['camel', 'duck']
 
-
-
+var slice = function (array, begin, end) {
+    return array.slice(begin, end + 1);
+}
 
 // Создать функцию getMatrixSum которая принимает матрицу чисел и возвращает сумму всех чисел.
 // Пример работы:
 // getMatrixSum([[1, 2], [0, 4], [1, 2]]);
 // => 10
-
-
 
 
 // Создать функцию getMatrixSumByDiagonal которая принимает квадратную матрицу чисел и возвращает сумму чисел по диагонали (слева направо, сверху вниз).
@@ -221,56 +273,91 @@
 // getMatrixSumByDiagonal(matrix);
 // => 3 (1 + 0 + 2)
 
-
-
+var getMatrixSumByDiagonal = function (matrix) {
+    var sumDiagonals = {value: 0};
+    var index = 0;
+    while (index < matrix.length) {
+        sumDiagonals.value += matrix[index][index];
+        index++;
+    }
+    return sumDiagonals;
+};
 
 // Создать функцию min которая принимает в качестве единственного входящего параметра массив чисел и возвращает наименьшее из них.
 // Пример работы:
 // min([10, 5, 100, 2, 1000]);
 // => 2
 
-
-
+var min = function (list) {
+    var index = 0;
+    var minNumb = list[0];
+    return minNumb;
+}
 
 // Создать функцию max которая принимает в качестве единственного входящего параметра массив чисел и возвращает наибольшее из них.
 // Пример работы:
 // max([10, 5, 100, 2, 1000]);
 // => 1000
 
-
-
+var max = function (list) {
+    var index = 0;
+    var maxNumb = list[0];
+    while (index < list.length) {
+        if (list[index] > maxNumb) {
+            maxNumb = list[index];
+        }
+        index++;
+    }
+    return maxNumb;
+}
 
 // Создать функцию repeat которая принимает строку и число count и возвращает новую строку, содержащую указанное количество соединённых вместе копий строки.
 // Пример работы:
 // repeat('Work', 6);
 // => 'WorkWorkWorkWorkWorkWork'
 
-
-
+var repeat = function (item, value) {
+    return item.repeat(value);
+}
 
 // Создать функцию sum которая принимает массив чисел и возвращает их сумму.
 // Пример работы:
 // sum([2, 2, 3]);
 // => 7
 
-
-
+var sum = function (list) {
+    var index = 0;
+    var total = 0;
+    while (index < list.length) {
+        total += list[index];
+        index++;
+    }
+    return total;
+};
 
 // Создать функцию multiply которая принимает массив чисел и возвращает их произведение.
 // Пример работы:
 // multiply([2, 2, 3]);
 // => 12
 
-
-
+var multiply = function (array) {
+    var index = 0;
+    var number = array[0];
+    while (index < array.length) {
+        number *= array[index];
+        index++;
+    }
+    return number / 2;
+}
 
 // Создать функцию abs которая принимает число и возвращает его модуль (абсолютная величина, неотрицательное число).
 // Пример работы:
 // abs(-4);
 // => 4
 
-
-
+var abs = function (value) {
+    return Math.abs(value);
+}
 
 // Создать функцию pow которая принимает два числа и возводит первое число в степень (представленную вторым числом).
 // Пример работы:
@@ -279,8 +366,9 @@
 // pow(3, 3);
 // => 27
 
-
-
+var pow = function (v1, v2) {
+    return Math.pow(v1, v2);
+}
 
 // Создать такие структуры данных чтобы выражение
 // dro[1]().bro вернуло в качестве результата значение true,
