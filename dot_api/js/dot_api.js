37e50e09d7ab51223de32f0fd9b420508602d94e
NickMostovoi <nick211996@gmail.com>
2021-12-04 16:57:05 +0300

    dot_api: done

--- dot_api/js/dot_api.js
+++ dot_api/js/dot_api.js
@@ -1,2 +1,23 @@
 // Write your code here
 console.log('Hello world!');
+
+$('.add-games').on('click', function() {
+    $('#games').html(doT.template($('#game-template').html())({games: games}));
+});
+
+$('.btn-nav-en').on('click', function() {
+    $('#nav').html(doT.template($('#nav-template').html())(navEn));
+});
+
+$('.btn-nav-ru').on('click', function() {
+    $('#nav').html(doT.template($('#nav-template').html())(navRu));
+});
+
+$('.add-whores').on('click', function() {
+    $('#whores').html(doT.template($('#whore-template').html())({whores: whores}));
+});
+
+$('.add-movies').on('click', function() {
+    $('#movies').html(doT.template($('#movie-template').html())({movies: movies}));
+});
+
