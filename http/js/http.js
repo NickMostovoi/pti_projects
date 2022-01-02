c07235c420b0367f8d98d0b57b28a2a84bb78665
NickMostovoi <nick211996@gmail.com>
2022-01-02 17:14:50 +0200

    http: done

--- http/js/http.js
+++ http/js/http.js
@@ -1,4 +1,78 @@
-// Write your code here
-console.log('Hello world!');
+$('.add-games').on('click', function() {
+    $.ajax({
+        method: 'GET',
+        url: 'http://127.0.0.1:3000/games',
+        dataType: 'json'
+    }).done(function(data) {
+        console.log('success', data);
+        $('#games').html(doT.template($('#game-template').html())({games: data}));
+    }).fail(function() {
+        console.log('error');
+    }).always(function() {
+        console.log('complete');
+    });
+});
 
-// Чередуй два способа (XMLHttpRequest и $.ajax)
+$('.btn-nav-en').on('click', function() {
+    var xhr = new XMLHttpRequest;
+    xhr.open('GET', 'http://127.0.0.1:3000/menu/en');
+    xhr.addEventListener('readystatechange', function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log('success', xhr.responseText);
+                $('#nav').html(doT.template($('#nav-template').html())(JSON.parse(xhr.responseText)));
+            } else {
+                console.log('error');
+            }
+            console.log('complete');
+        }
+    });
+    xhr.send();
+});
+
+$('.btn-nav-ru').on('click', function() {
+    $.ajax({
+        method: 'GET',
+        url: 'http://127.0.0.1:3000/menu/ru',
+        dataType: 'json'
+    }).done(function(data) {
+        console.log('success', data);
+        $('#nav').html(doT.template($('#nav-template').html())(data));
+    }).fail(function() {
+        console.log('error');
+    }).always(function() {
+        console.log('complete');
+    });
+});
+
+$('.add-whores').on('click', function() {
+    var xhr = new XMLHttpRequest;
+    xhr.open('GET', 'http://127.0.0.1:3000/whores');
+    xhr.addEventListener('readystatechange', function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log('success', xhr.responseText);
+                $('#whores').html(doT.template($('#whore-template').html())({whores: JSON.parse(xhr.responseText)}));
+            } else {
+                console.log('error');
+            }
+            console.log('complete');
+        }
+    });
+    xhr.send();
+});
+
+$('.add-movies').on('click', function() {
+    $.ajax({
+        method: 'GET',
+        url: 'http://127.0.0.1:3000/movies',
+        dataType: 'json'
+    }).done(function(data) {
+        console.log('success', data);
+        $('#movies').html(doT.template($('#movie-template').html())({movies: data}));
+    }).fail(function() {
+        console.log('error');
+    }).always(function() {
+        console.log('complete');
+    });
+});
